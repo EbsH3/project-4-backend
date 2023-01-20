56aa231cc2825f0eca932f862bc34388a8bbd576
Ebrar Hassan <ebrarhassan93@gmail.com>
2023-01-20 16:09:37 +0000

    adding employer seeds

--- makeseeds.sh
+++ makeseeds.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 echo "creating employers/seeds.json"
-python manage.py dumpdata employers --output albums/seeds.json --indent=2;
+python manage.py dumpdata employers --output employers/seeds.json --indent=2;
 
 echo "creating salaries/seeds.json"
-python manage.py dumpdata salaries --output artists/seeds.json --indent=2;
+python manage.py dumpdata salaries --output salaries/seeds.json --indent=2;
 
 echo "creating sectors/seeds.json"
-python manage.py dumpdata sectors --output genres/seeds.json --indent=2;
+python manage.py dumpdata sectors --output sectors/seeds.json --indent=2;
 
 echo "creating tips/seeds.json"
-python manage.py dumpdata tips --output comments/seeds.json --indent=2;
+python manage.py dumpdata tips --output tips/seeds.json --indent=2;
 
 echo "creating feedback/seeds.json"
-python manage.py dumpdata feedback --output comments/seeds.json --indent=2;
+python manage.py dumpdata feedback --output feedback/seeds.json --indent=2;
 
 echo "creating vacancies/seeds.json"
-python manage.py dumpdata vacancies --output comments/seeds.json --indent=2;
+python manage.py dumpdata vacancies --output vacancies/seeds.json --indent=2;
 
 
 echo "creating jwt_auth/seeds.json"
