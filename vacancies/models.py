236e67e60c183db58ee73e4807a6e77c5f54fc1a
Ebrar Hassan <ebrarhassan93@gmail.com>
2023-01-24 21:21:11 +0000

    seeds

--- vacancies/models.py
+++ vacancies/models.py
@@ -4,6 +4,7 @@ class Vacancy(models.Model):
   title = models.CharField(max_length=50)
   location = models.CharField(max_length=50)
   employer = models.ManyToManyField('employers.Employer', related_name="vacancies")
+  salary = models.CharField(max_length=50)
   url = models.CharField(max_length=700)
   
   def __str__(self):
